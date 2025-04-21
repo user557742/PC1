76918ef13ccc1cbf52c69f1f2c38cf6779e6c4c7
user557742 <longduonggia304@gmail.com>
2025-04-21 19:55:49 +0700

    Create C-poster.cpp

---
+++ segtree/C-poster.cpp
@@ -0,0 +1,25 @@
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+const ll inf = 1e18;
+const int mod=1e9+7;
+const int maxn = 1e6+1;
+int n;
+int w[maxn],h[maxn];
+int r[maxn];
+int sz;
+struct it{
+    vector<int> g;
+    void init(int n){
+        g.resize(4*n+1,0);
+        sz=n;
+    }
+    void upd(int u)
+};
+int main(){
+    ios::sync_with_stdio(0);cin.tie(0);cout.tie(0);
+
+
+
+    return 0;
+}
