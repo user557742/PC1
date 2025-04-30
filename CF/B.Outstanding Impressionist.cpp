0d535a675171bdb5ed9b536a67f7efda6c0bf811
user557742 <longduonggia304@gmail.com>
2025-04-30 15:51:15 +0700

    Create B.Outstanding Impressionist.cpp

---
+++ CF/B.Outstanding Impressionist.cpp
@@ -0,0 +1,16 @@
+unsolved
+#include <bits/stdc++.h> 
+using namespace std;
+typedef long long ll;
+const int maxn =1e6+1;
+const int mod = 1e9+7;
+#define mp make_pair
+#define pb push_back
+#define rei(i,a,b) for(int i=a;i<=b;i++)
+#define red(i,a,b) for(int i=a;i>=b;i--)
+#define fi first
+#define se second
+int main(){
+    ios::sync_with_stdio(0);cin.tie(0);cout.tie(0);
+    return 0;
+}
