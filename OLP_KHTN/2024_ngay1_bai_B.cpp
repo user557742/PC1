341306cb421e469c0964b2cdcf5fe4a80e8da731
user557742 <longduonggia304@gmail.com>
2025-05-02 19:24:00 +0700

    Create 2024_ngay1_bai_B.cpp

---
+++ OLP_KHTN/2024_ngay1_bai_B.cpp
@@ -0,0 +1,22 @@
+// De bai : cho cây n dỉnh một cách được gọi là hợp lệ 
+// nếu hiệu số đỉnh đen trừ số đỉnh trắng trên cây hông vượt quá 1 trên mọi cây con
+// nháp :
+/*
+    Nếu biểu điễn cây thành mảng thì các đỉnh thuôc cây con gôc u nằm từ bd[u] đến kt[u]
+    dp-bitmask canh u,v 
+*/
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+const int maxn =1e6+1;
+const int mod = 998244353;
+#define mp make_pair
+#define pb push_back
+#define rei(i,a,b) for(int i=a;i<=b;i++)
+#define red(i,a,b) for(int i=a;i>=b;i--)
+#define fi first
+#define se second
+int main(){
+    ios::sync_with_stdio(0);cin.tie(0);cout.tie(0);
+    return 0;
+}
