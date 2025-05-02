92aabdb9103f1e8863af7ab05498effb627a4499
user557742 <longduonggia304@gmail.com>
2025-05-02 19:23:10 +0700

    Create A. Permutation Warm-Up.cpp

---
+++ CF/A. Permutation Warm-Up.cpp
@@ -0,0 +1,13 @@
+// đề bài : cho một số n và hàm f(P)=|p[i]-i| với P là một tập hoán vị từ 1 đến n 
+// hãy đếm xem có bao nhiêu giá trị khác nhau . giới hạn : n <= 500
+/*
+    nháp: n = 3
+    1 2 3
+    1 3 2 :2 
+    2 1 3 :2 
+    2 3 1 :3
+    3 2 1 :4
+    3 1 2 :4
+    giải nén 
+
+*/
